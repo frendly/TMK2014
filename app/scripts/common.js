1000ffba613ecff9de820100a0a634e65ebec0a8
frendly <www.zpdf@mail.ru>
2015-09-10 17:25:27 +0300

    add accordion module

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -7,6 +7,7 @@ $(function () {
 	queryToInput();
 	prevNextLink();
 	lightbox();
+	accordion();
 
 	currentLinkToHistory();
 });
@@ -149,8 +150,23 @@ function prevNextLink() {
 	$('.prev-next-navigation').prepend(prevPageLink, nextPageLink).sticky({topSpacing: 20}); /* STICKY BLOCK */
 }
 
-/* HISTORY FUNCTION */
+function accordion() {
+	var acc_heading = $('.acc_heading');
 
+	acc_heading.click(function() {
+		if ($(this).hasClass('acc_open')) {
+			$(this).removeClass('acc_open').addClass('acc_closed').next().hide();
+			return false;
+		} else {
+			$(this).removeClass('acc_closed').addClass('acc_open').next().show();
+			return true;
+		}
+	});
+
+	acc_heading.addClass('acc_closed').next().hide();
+}
+
+/* HISTORY FUNCTION */
 function currentLinkToHistory() {
 	var title = document.title,
 		href = document.location.href,
